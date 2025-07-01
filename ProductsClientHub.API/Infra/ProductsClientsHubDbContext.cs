e4284e5207c25f4bbb7db074814202110bc53469
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-07-01 15:33:31 -0300

    alterações

---
+++ ProductsClientHub.API/Infra/ProductsClientsHubDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsClientHub.API.Entities;
+
+namespace ProductsClientHub.API.Infra;
+
+public class ProductsClientsHubDbContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\ferna\\Downloads\\1737062251373-attachment.db");
+    }
+}
--- ProductsClientHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
+++ ProductsClientHub.API/UseCases/Clients/Register/RegisterClientUseCase.cs
@@ -1,4 +1,6 @@
-﻿using ProductsClientHub.Communication.Requests;
+﻿using ProductsClientHub.API.Entities;
+using ProductsClientHub.API.Infra;
+using ProductsClientHub.Communication.Requests;
 using ProductsClientHub.Communication.Responses;
 using ProductsClientHub.Exceptions.ExceptionsBase;
 
@@ -7,6 +9,29 @@ namespace ProductsClientHub.API.UseCases.Clients.Register;
 public class RegisterClientUseCase
 {
     public ResponseClientJson Execute(RequestClientJson request)
+    {
+        Validate(request);
+
+        var dbContext = new ProductsClientsHubDbContext();
+
+        var entity = new Client()
+        {
+            Name = request.Name,
+            Email = request.Email,
+        };
+
+        dbContext.Clients.Add(entity);
+
+        dbContext.SaveChanges();
+
+        return new ResponseClientJson()
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+        };
+    }
+
+    private void Validate(RequestClientJson request)
     {
         var validator = new RegisterClientValidator();
 
@@ -18,7 +43,5 @@ public class RegisterClientUseCase
 
             throw new ErrorOnValidationException(errors);
         }
-
-        return new ResponseClientJson();
     }
 }
